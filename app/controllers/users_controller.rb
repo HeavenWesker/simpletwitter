b4ec3b1bb0fd73bc42f53b277292629438adb2bb
Heaven Wesker <Heaven.Wesker@gmail.com>
2014-09-09 08:46:37 +0800

    improve

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,27 @@ class UsersController < ApplicationController
   before_filter :correct_user,    only: [:edit, :update]
   before_filter :admin_user,      only: :destroy
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App"
-      redirect_to @user
+    if !signed_in?
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App"
+        redirect_to @user
+      else
+        render "new"
+      end
     else
-      render "new"
+      flash[:notice] = "There is no need to Sign up"
+      redirect_to root_path
     end
   end
   def new
-    @user = User.new
+    if signed_in?
+      flash[:notice] = "There is no need to Sign up"
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
   def show
     @user = User.find(params[:id])
