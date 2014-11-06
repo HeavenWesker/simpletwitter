05162ba8621755fba9ad679b721ac6a187b19b9f
Heaven Wesker <Heaven.Wesker@gmail.com>
2014-11-06 15:49:16 +0800

    bug fixed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,6 @@ class Micropost < ActiveRecord::Base
   validates :content, length: { maximum: 140 }, presence: true
   default_scope order: 'microposts.created_at DESC'
   def self.from_users_followed_by(user)
-    #followed_user_ids = user.followed_user_ids
     followed_user_ids = "select followed_id from relationships where follower_id = :user_id"
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
   end
